cb2da471ffe7184c993b3b8d06fc13bf74db6670
DavisDavisDavis <olizoooom@gmail.com>
2021-12-15 10:30:09 +0100

    took code from previous project (stolen code) ✨🐱

---
+++ app/users/login.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../autoload.php';
+echo "hewwo world <br>";
+// In this file we login users.
+if (isset($_POST['email'], $_POST['password'])) {
+    echo "it works <3 <br>";
+
+    $statment = $database->query('SELECT * FROM users');
+    $user = $statment->fetch(PDO::FETCH_ASSOC);
+
+
+    if ($user['email'] === $_POST['email'] && password_verify($_POST['password'], $user['password'] )) {
+        echo "user name correct 💖";
+        $_SESSION['user'] = $user;
+    }
+
+    redirect('/');
+}
+
+redirect('/');
