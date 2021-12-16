d4e1e6f188eec7a3f9aea682a922bf05b2fad078
DavisDavisDavis <olizoooom@gmail.com>
2021-12-16 16:13:43 +0100

    setting up databse on logic for user list creation

--- lists.php
+++ lists.php
@@ -1,10 +1,17 @@
 <?php require __DIR__ . '/app/autoload.php'; ?>
 <?php require __DIR__ . '/views/header.php'; ?>
+<?php
+
+$statment = $database->query('SELECT * FROM lists');
+$lists = $statment->fetch(PDO::FETCH_ASSOC);
+
+?>
 
 <main>
     <h1>Task list</h1>
 
-    <script src="task-list.js"></script>
+
+    <script src="list.js"></script>
 </main>
 
 <?php require __DIR__ . '/views/footer.php'; ?>
