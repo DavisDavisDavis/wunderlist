65f9b37148fec9b72d606a70131cafb87f6111fb
DavisDavisDavis <olizoooom@gmail.com>
2021-12-20 21:39:41 +0100

    i didnt fix anything
    the posts had been working the entire time and i just didnt refresh my table plus

--- lists.php
+++ lists.php
@@ -2,26 +2,27 @@
 <?php require __DIR__ . '/views/header.php'; ?>
 <?php
 
-$statment = $database->query('SELECT * FROM posts');
-
-$posts = $statment->fetch(PDO::FETCH_ASSOC);
 
 if (isset($_POST['title'], $_POST['content'])) {
     echo $_POST['title'] . "<br>" . $_POST['content'];
+    $author_id = 6;
 
+    $statment = $database->query('SELECT * FROM posts');
 
+    $posts = $statment->fetch(PDO::FETCH_ASSOC);
 
     $query =
-        'INSERT INTO posts (id, title, content, author_id)
-        VALUES (2, :title, :content, 2)';
+        'INSERT INTO posts (title, content, author_id)
+        VALUES (:title, :content, :author_id)';
 
     $insert = $database->prepare($query);
 
     $insert->bindParam(':title', $_POST['title'], PDO::PARAM_STR);
     $insert->bindParam(':content', $_POST['content'], PDO::PARAM_STR);
-
+    $insert->bindParam(':author_id', $author_id, PDO::PARAM_INT);
 
     $insert->execute();
+
     foreach ($posts as $post) {
         echo $post;
     }
