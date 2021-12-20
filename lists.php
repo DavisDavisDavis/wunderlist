1bb32572e7e69042ff6f25242cb6acf186ba0e6a
DavisDavisDavis <olizoooom@gmail.com>
2021-12-20 19:10:52 +0100

    ++ lists.php post logic
    trying to figure out how to insert user submited data

--- lists.php
+++ lists.php
@@ -2,14 +2,48 @@
 <?php require __DIR__ . '/views/header.php'; ?>
 <?php
 
-$statment = $database->query('SELECT * FROM lists');
-$lists = $statment->fetch(PDO::FETCH_ASSOC);
+$statment = $database->query('SELECT * FROM posts');
 
+$posts = $statment->fetch(PDO::FETCH_ASSOC);
+
+if (isset($_POST['title'], $_POST['content'])) {
+    echo $_POST['title'] . "<br>" . $_POST['content'];
+
+
+
+    $query =
+        'INSERT INTO posts (id, title, content, author_id)
+        VALUES (2, :title, :content, 2)';
+
+    $insert = $database->prepare($query);
+
+    $insert->bindParam(':title', $_POST['title'], PDO::PARAM_STR);
+    $insert->bindParam(':content', $_POST['content'], PDO::PARAM_STR);
+
+
+    $insert->execute();
+    foreach ($posts as $post) {
+        echo $post;
+    }
+}
 ?>
 
 <main>
     <h1>Task list</h1>
 
+    <form action="" method="post">
+        <div class="mb-3">
+            <label for="title">title</label>
+            <input class="" type="title" name="title" id="title" placeholder="title" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="content">content</label>
+            <input class="" type="content" name="content" id="content" required>
+        </div>
+
+        <button type="submit" class="btn btn-primary">submit</button>
+    </form>
 
     <script src="list.js"></script>
 </main>
