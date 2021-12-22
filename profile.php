ef72c27faf6b98ee3d29abe5ead9f50b1685cc32
DavisDavisDavis <olizoooom@gmail.com>
2021-12-22 15:28:20 +0100

    some logic for profile
    not finished at all but the start 👍

--- profile.php
+++ profile.php
@@ -1,6 +1,28 @@
 <?php require __DIR__ . '/app/autoload.php'; ?>
 <?php require __DIR__ . '/views/header.php'; ?>
 
+<?php
+//Resetting _FILES
+$_POST['title'] = NULL;
+
+if (isset($_FILES['avatar'])) {
+    $statment = $database->query('SELECT name FROM users');
+    $names = $statment->fetch(PDO::FETCH_ASSOC);
+
+    echo $names['name'];
+
+    $avatar = $_FILES['avatar'];
+
+    die(var_dump($avatar['tmp_name']));
+
+    move_uploaded_file($avatar['tmp_name'], __DIR__ . '/' . $names);
+}
+
+
+
+?>
+
+
 <main>
     <h1>Profile</h1>
 
