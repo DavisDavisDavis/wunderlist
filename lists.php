a8ed4d224a38eaf08a772fcd162dbbf29e474aff
DavisDavisDavis <olizoooom@gmail.com>
2022-01-08 17:41:08 +0200

    post to database
    trying to add values from _POST to the database

--- lists.php
+++ lists.php
@@ -9,22 +9,31 @@ foreach ($posts as $post) {
     echo $post['title'] . $post['content'] . '<br>';
 }
 
-$_POST['title'] = NULL;
-$_POST['content'] = NULL;
+// $_POST['title'] = NULL;
+// $_POST['content'] = NULL;
+// $_POST['completed'] = NULL;
+// $_POST['deadline'] = NULL;
+
 
 if (isset($_POST['title'], $_POST['content'])) {
-    $author_id = 6;
+    echo "it went through 💖";
+
 
 
     $query =
-        'INSERT INTO posts (title, content, author_id)
-        VALUES (:title, :content, :author_id)';
+        'INSERT INTO lists (lists_id, user_id, title, description, completed, deadline)
+        VALUES (:lists_id, :user_id, :title, :content, :completed, :deadline)';
 
     $insert = $database->prepare($query);
 
+    $insert->bindParam(':lists_id', $_POST['lists_id'], PDO::PARAM_STR);
+    $insert->bindParam(':user_id', $_POST['user_id'], PDO::PARAM_STR);
     $insert->bindParam(':title', $_POST['title'], PDO::PARAM_STR);
     $insert->bindParam(':content', $_POST['content'], PDO::PARAM_STR);
-    $insert->bindParam(':author_id', $author_id, PDO::PARAM_INT);
+    $insert->bindParam(':completed', $_POST['completed'], PDO::PARAM_BOOL);
+    $insert->bindParam(':deadline', $_POST['deadline'], PDO::PARAM_STR);
+
+    // $insert->bindParam(':author_id', $author_id, PDO::PARAM_INT);
 
     $insert->execute();
 
@@ -49,12 +58,12 @@ if (isset($_POST['title'], $_POST['content'])) {
 
         <div>
             <label for="checkbox">completed</label>
-            <input type="checkbox">
+            <input type="checkbox" name="completed">
         </div>
 
         <div>
             <label for="checkbox">deadline</label>
-            <input type="date">
+            <input type="date" name="deadline">
         </div>
 
 
