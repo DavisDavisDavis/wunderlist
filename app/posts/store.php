a68271131740a6dd809aeb80864d3566b3d4a9f7
DavisDavisDavis <olizoooom@gmail.com>
2022-01-12 01:42:08 +0200

    cleaned it up!

--- app/posts/store.php
+++ app/posts/store.php
@@ -5,5 +5,60 @@ declare(strict_types=1);
 require __DIR__ . '/../autoload.php';
 
 // In this file we store/insert new posts in the database.
+$statment_lists = $database->query('SELECT * FROM lists');
 
-redirect('/');
+
+$_POST['title'] = NULL;
+$_POST['description'] = NULL;
+$_POST['completed'] = NULL;
+$_POST['deadline'] = NULL;
+
+if (isset($_POST['title'], $_POST['content'])) {
+    echo "it went through 💖";
+
+
+    $_POST['lists_id'] = 3;
+
+
+    $query =
+        'INSERT INTO lists (lists_id, user_id, title, description, completed, deadline)
+        VALUES (:lists_id, :user_id, :title, :content, :completed, :deadline);';
+
+    $insert = $database->prepare($query);
+
+    $insert->bindParam(':lists_id', $_POST['lists_id'], PDO::PARAM_STR);
+    $insert->bindParam(':user_id', $_SESSION['email'], PDO::PARAM_STR);
+    $insert->bindParam(':title', $_POST['title'], PDO::PARAM_STR);
+    $insert->bindParam(':content', $_POST['content'], PDO::PARAM_STR);
+    $insert->bindParam(':deadline', $_POST['deadline'], PDO::PARAM_STR);
+
+    $insert->bindParam(':completed', $_POST['complete'], PDO::PARAM_BOOL);
+
+    // if ($_POST['completed']) {
+    //     $insert->bindParam(':completed', true, PDO::PARAM_BOOL);
+    // } else {
+    //     $insert->bindParam(':completed', false, PDO::PARAM_BOOL);
+    // }
+
+    // $insert->bindParam(':author_id', $author_id, PDO::PARAM_INT);
+
+    $insert->execute();
+
+    //Resetting _POST
+}
+
+if (isset($_POST['page_name'])) {
+    echo "it went through 💖";
+
+    $query_page = 'INSERT INTO pages(user_id, page_title)
+    VALUES (:user_id, :page_title);';
+
+    $insert_pages = $database->prepare($query_page);
+
+    $insert_pages->bindParam(':user_id', $_SESSION['email'], PDO::PARAM_STR);
+    $insert_pages->bindParam(':page_title', $_POST['page_name'], PDO::PARAM_STR);
+
+    $insert_pages->execute();
+}
+
+redirect('/lists.php');
--- lists.php
+++ lists.php
@@ -1,69 +1,18 @@
 <?php require __DIR__ . '/app/autoload.php'; ?>
 <?php require __DIR__ . '/views/header.php'; ?>
+
 <?php
+
 $statment_lists = $database->query('SELECT * FROM lists');
 
 $lists = $statment_lists->fetchAll(PDO::FETCH_ASSOC);
-
-$_POST['title'] = NULL;
-$_POST['description'] = NULL;
-$_POST['completed'] = NULL;
-$_POST['deadline'] = NULL;
-
-if (isset($_POST['title'], $_POST['content'])) {
-    echo "it went through 💖";
-
-
-    $_POST['lists_id'] = 3;
-
-
-    $query =
-        'INSERT INTO lists (lists_id, user_id, title, description, completed, deadline)
-        VALUES (:lists_id, :user_id, :title, :content, :completed, :deadline);';
-
-    $insert = $database->prepare($query);
-
-    $insert->bindParam(':lists_id', $_POST['lists_id'], PDO::PARAM_STR);
-    $insert->bindParam(':user_id', $_SESSION['email'], PDO::PARAM_STR);
-    $insert->bindParam(':title', $_POST['title'], PDO::PARAM_STR);
-    $insert->bindParam(':content', $_POST['content'], PDO::PARAM_STR);
-    $insert->bindParam(':deadline', $_POST['deadline'], PDO::PARAM_STR);
-
-    $insert->bindParam(':completed', $_POST['complete'], PDO::PARAM_BOOL);
-
-    // if ($_POST['completed']) {
-    //     $insert->bindParam(':completed', true, PDO::PARAM_BOOL);
-    // } else {
-    //     $insert->bindParam(':completed', false, PDO::PARAM_BOOL);
-    // }
-
-    // $insert->bindParam(':author_id', $author_id, PDO::PARAM_INT);
-
-    $insert->execute();
-
-    //Resetting _POST
-}
-
-if (isset($_POST['page_name'])) {
-    echo "it went through 💖";
-
-    $query_page = 'INSERT INTO pages(user_id, page_title)
-    VALUES (:user_id, :page_title);';
-
-    $insert_pages = $database->prepare($query_page);
-
-    $insert_pages->bindParam(':user_id', $_SESSION['email'], PDO::PARAM_STR);
-    $insert_pages->bindParam(':page_title', $_POST['page_name'], PDO::PARAM_STR);
-
-    $insert_pages->execute();
-}
 
 ?>
 
 <main>
     <h1>Task list</h1>
 
-    <form action="" method="POST">
+    <form action="/app/posts/store.php" method="POST">
         <h2>Create a page</h2>
 
         <div class="mb-3">
@@ -82,7 +31,7 @@ if (isset($_POST['page_name'])) {
         </div>
     <?php endforeach; ?>
 
-    <form action="" method="post">
+    <form action="/app/posts/store.php" method="post">
         <div class="mb-3">
             <label for="title">title</label>
             <input class="" type="title" name="title" id="title" placeholder="title" required>
