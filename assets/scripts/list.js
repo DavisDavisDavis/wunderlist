5d0b22d58d6c2d40946cab4e3f09370d5d64f133
DavisDavisDavis <olizoooom@gmail.com>
2022-01-13 13:08:15 +0200

    lint

--- assets/scripts/list.js
+++ assets/scripts/list.js
@@ -1,7 +1,7 @@
-const buttons = document.querySelectorAll('.tool_list button');
-const forms = document.querySelectorAll('.lists forms');
+// const buttons = document.querySelectorAll('.tool_list button');
+// const forms = document.querySelectorAll('.lists forms');
 
-function display_form(e, formID) {
-  document.querySelectorAll('form').forEach(form => form.style.display = 'none';
-  document.getElementById('#').style.display = 'block';
-}
+// function display_form(e, formID) {
+//   document.querySelectorAll('form').forEach(form => form.style.display = 'none'
+//   document.getElementById('#').style.display = 'block';
+// }
